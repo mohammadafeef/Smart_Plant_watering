2df312899854402d9ea2b27c5cc4aa0988f6c815
Mohammad Afeef <mohammadafeef784@gmail.com>
2023-10-12 11:07:48 +0530

    modifed a set function with space input

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -3,20 +3,37 @@
 //A global time variable
 RTC_DS3231 rtc;
 
+int SerialreadInt(){
+  int num = 0; 
+  while(1){
+    if(Serial.available()){
+
+      char info = Serial.read();
+
+      if(info>='0'&&info<='9')
+        num = (num*10)+(info-'0');
+
+      else if(info == ' ')
+        return num;
+    }
+  }
+}
+
 void set(){
   //This function will let you to set the time through serial port
-  Serial.print("Enter the Year: ");
-  int year = Serial.read();
-  Serial.print("Enter the Month: ");
-  int month = Serial.read();
-  Serial.print("Enter the date: ");
-  int date = Serial.read();
-  Serial.print("Enter the Hour: ");
-  int hour = Serial.read();
-  Serial.print("Enter the Minute: ");
-  int minute = Serial.read();
-  Serial.print("Enter the Seconds: ");
-  int seconds = Serial.read();
+  Serial.println("Setting the RTC Activated");
+  Serial.print("\nEnter the Year: ");
+  int year = SerialreadInt();
+  Serial.print("\nEnter the Month: ");
+  int month = SerialreadInt();
+  Serial.print("\nEnter the date: ");
+  int date = SerialreadInt();
+  Serial.print("\nEnter the Hour: ");
+  int hour = SerialreadInt();
+  Serial.print("\nEnter the Minute: ");
+  int minute = SerialreadInt();
+  Serial.print("\nEnter the Seconds: ");
+  int seconds = SerialreadInt();
   //setting the time in this function
   rtc.adjust(DateTime(year, month, date, hour, minute, seconds));
 }
@@ -28,5 +45,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //To activate the set function '!' is sent
+  if(Serial.available()>0){
+    char info = Serial.read();
+    if(info == '!')
+      set();
+  }
+
 
 }
