a61951ae276d481405f7a9c39b72ee4d7bcb568e
Mohammad Afeef <mohammadafeef784@gmail.com>
2023-10-12 11:46:42 +0530

    modifed a set function with input

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -4,7 +4,7 @@
 RTC_DS3231 rtc;
 
 int SerialreadInt(){
-  int num = 0; 
+  int num = 0;
   while(1){
     if(Serial.available()){
 
@@ -13,8 +13,10 @@ int SerialreadInt(){
       if(info>='0'&&info<='9')
         num = (num*10)+(info-'0');
 
-      else if(info == ' ')
+      else if(info == '\n'){
+        Serial.println(num);
         return num;
+      }
     }
   }
 }
@@ -22,25 +24,35 @@ int SerialreadInt(){
 void set(){
   //This function will let you to set the time through serial port
   Serial.println("Setting the RTC Activated");
-  Serial.print("\nEnter the Year: ");
+  Serial.print("Enter the Year: ");
   int year = SerialreadInt();
-  Serial.print("\nEnter the Month: ");
+  Serial.print("Enter the Month: ");
   int month = SerialreadInt();
-  Serial.print("\nEnter the date: ");
+  Serial.print("Enter the date: ");
   int date = SerialreadInt();
-  Serial.print("\nEnter the Hour: ");
+  Serial.print("Enter the Hour: ");
   int hour = SerialreadInt();
-  Serial.print("\nEnter the Minute: ");
+  Serial.print("Enter the Minute: ");
   int minute = SerialreadInt();
-  Serial.print("\nEnter the Seconds: ");
+  Serial.print("Enter the Seconds: ");
   int seconds = SerialreadInt();
   //setting the time in this function
   rtc.adjust(DateTime(year, month, date, hour, minute, seconds));
+  Serial.println("Time set completed!");
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 void loop() {
@@ -48,9 +60,27 @@ void loop() {
   //To activate the set function '!' is sent
   if(Serial.available()>0){
     char info = Serial.read();
-    if(info == '!')
+    if(info == '!'){
+      while(Serial.read()!='\n'); // Discard any remaining characters in the input buffer
       set();
+    }
   }
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  delay(1000);
 
 
 }
